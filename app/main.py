ca543f3f81fe1b2579c3f539c53bd1d3185c03fc
code-project <dev@code-check.io>
2018-06-04 13:41:34 +0000

    Add new CLI related files

---
+++ app/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def main(argv):
+    # このコードは引数と標準出力を用いたサンプルコードです。
+    # このコードは好きなように編集・削除してもらって構いません。
+    # ---
+    # This is a sample code to use arguments and outputs.
+    # Edit and remove this code as you like.
+
+    for i, v in enumerate(argv):
+        print("argv[{0}]: {1}".format(i, v))
